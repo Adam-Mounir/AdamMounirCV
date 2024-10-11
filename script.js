8b3a60898e50a4a57f521834688a5f6519cf88d4
Adam-Mounir <adam.g.mounir@gmail.com>
2024-10-11 09:33:35 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Functionality for the download button
+document.querySelector('.download_print_buttons a').addEventListener('click', function(event) {
+    event.preventDefault();
+    alert('Download CV functionality to be implemented.');
+});
